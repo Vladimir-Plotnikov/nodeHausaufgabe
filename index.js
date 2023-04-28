67373170dfdfe373ec49d0bc2da237ecc608b8d2
Volodymyr <vovdata@gmail.com>
2023-04-28 21:31:30 +0200

    work place

---
+++ index.js
@@ -0,0 +1,8 @@
+const obj = require("./users")
+// console.log(obj);
+
+const {admins}  = require("./users");
+
+console.log(admins);
+
+console.log('Welcome to Sladiusik and ruchki terebuchki');
---
+++ users.js
@@ -0,0 +1,22 @@
+// const { moduleExpression } = require("@babel/types");
+
+const admins = [
+    "Alex",
+    "Andrey",
+    "Vasiliy"
+];
+
+const clients = [
+    "Anna",
+    "Alina",
+    "Tamara"
+]
+
+const users = {
+    admins,
+    clients
+}
+
+
+
+module.exports = users;
